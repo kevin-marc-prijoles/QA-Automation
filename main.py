29a6c8c9736b590e4bb7d4b7b5b690ab1871cfbf
Kevin Marc Prijoles <kevin@ronlinemarketing.com>
2022-07-23 01:20:19 +0800

    testing username changes on my local machine

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 
 os.environ['PATH'] += r"C:/selenium_driver"
 driver = webdriver.Chrome()
+
+#This only goes to google.com nothing fancy lol
 driver.get("https://google.com")
